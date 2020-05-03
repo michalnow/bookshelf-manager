c943280911133f8b42104381bc273b37ff259483
Michał Nowakowski <michal951@poczta.onet.pl>
2020-05-03 12:43:14 +0200

    change server

--- server/src/main/java/com/bai/server/book/Book.java
+++ server/src/main/java/com/bai/server/book/Book.java
@@ -10,17 +10,24 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name= "book")
 public class Book {
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id")
     private Long id;
+    @Column(name="title")
     private String title;
     @Lob
     @Column(name="description", length=10000)
     private String description;
+    @Column(name="author")
     private String author;
+    @Column(name="pages")
     private int pages;
+    @Column(name="poster")
     private String poster;
+    @Column(name="genre")
     private String genre;
 }
